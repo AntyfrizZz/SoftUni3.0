584914e144955c2f86103054abe94c67b081907d
Yavor Milinov <iavor.milinov@gmail.com>
2014-05-27 22:24:51 +0300

    Update Problem04RectanglesPerimeterAndArea.cs

--- Courses/C#BasicsMach2014/Homework-AntyfrizZz-Operators-Expressions-and-Statements/Problem04RectanglesPerimeterAndArea.cs
+++ Courses/C#BasicsMach2014/Homework-AntyfrizZz-Operators-Expressions-and-Statements/Problem04RectanglesPerimeterAndArea.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 //Write an expression that calculates rectangle’s perimeter and area by given width and height.
 
+//https://softuni.bg/downloads/svn/csharp-basics/Feb-2014/3.%20Operators-Expressions-and-Statements-Homework.docx
+
 class RectanglesPerimeterAndArea
 {
     static void Main()
