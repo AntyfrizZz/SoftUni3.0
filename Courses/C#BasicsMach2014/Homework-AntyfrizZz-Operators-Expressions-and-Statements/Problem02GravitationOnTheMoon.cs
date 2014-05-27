a4f441070bd7efea35d38931e1dc4326c08f6439
Yavor Milinov <iavor.milinov@gmail.com>
2014-05-27 22:24:25 +0300

    Update Problem02GravitationOnTheMoon.cs

--- Courses/C#BasicsMach2014/Homework-AntyfrizZz-Operators-Expressions-and-Statements/Problem02GravitationOnTheMoon.cs
+++ Courses/C#BasicsMach2014/Homework-AntyfrizZz-Operators-Expressions-and-Statements/Problem02GravitationOnTheMoon.cs
@@ -2,6 +2,8 @@
 //The gravitational field of the Moon is approximately 17% of that on the Earth.
 //Write a program that calculates the weight of a man on the moon by a given weight on the Earth.
  
+//https://softuni.bg/downloads/svn/csharp-basics/Feb-2014/3.%20Operators-Expressions-and-Statements-Homework.docx
+ 
 class GravitationOnTheMoon
 {
     static void Main()
