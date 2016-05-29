fc8b6ebc9ed37c0aedc0bc5e78eaec73c68af1af
Yavor Milinov <iavor.milinov@gmail.com>
2016-05-30 00:49:43 +0300

    Algorithms-Exam-6-December-2015

---
+++ SoftUni 3.0/Algorithms-Exams-and-Exam-Exercices/Algorithms-Exam-6-December-2015/01.Sequences-of-Limited-Sum/Launch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Launch
+{
+    static StringBuilder output = new StringBuilder();
+    static List<int> numbers = new List<int>();
+    static void Main()
+    {
+        var s = int.Parse(Console.ReadLine());
+
+        GenerateSequences(0, s);
+
+        Console.WriteLine(output);
+    }
+
+    static void GenerateSequences(int sum, int max)
+    {      
+        for (int i = 1; i <= max; i++)
+        {
+            if (sum + i <= max)
+            {
+                numbers.Add(i);
+                Print(numbers);
+
+                GenerateSequences(sum + i, max);
+
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+            else
+            {
+                break;
+            }         
+        }
+    }
+
+    private static void Print(List<int> numbers)
+    {
+        output.AppendLine(string.Join(" ", numbers));
+    }
+}
---
+++ SoftUni 3.0/Algorithms-Exams-and-Exam-Exercices/Algorithms-Exam-6-December-2015/README.md
@@ -0,0 +1,3 @@
+Algorithms Exam - 6 December 2015
+=========================================================
+https://judge.softuni.bg/Contests/Practice/Index/142#0
