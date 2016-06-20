995c5bb84ef9d4b6a299154276b1ff90d8976ebe
Yavor Milinov <iavor.milinov@gmail.com>
2016-06-20 13:41:58 +0300

    Exam-19-June-2016 Added

---
+++ SoftUni 3.0/C#-Exams-and-Exam-Exercices/Exam-19-June-2016/01.Cubic-Artillery/Startup.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Startup
+{
+    static List<Bunker> bunkers = new List<Bunker>();
+
+    static Bunker currentBunker;
+    static Queue<Bunker> nextBunker = new Queue<Bunker>();
+
+    static bool foundBunker;
+
+    static StringBuilder result = new StringBuilder();
+
+    static void Main()
+    {
+        var totalCapacity = int.Parse(Console.ReadLine());
+
+        var inputLine = Console.ReadLine();
+
+        while (!inputLine.Equals("Bunker Revision"))
+        {
+            var elements = inputLine.Split();
+
+            foreach (var element in elements)
+            {
+                if (char.IsLetter(element[0])) //We have a bunker
+                {
+                    var tempBunker = new Bunker(element, 0, new List<int>());
+                    bunkers.Add(tempBunker); //Add it to our list
+
+                    if (currentBunker == null) // This is done to the first bunker. We mark it as current.
+                    {
+                        currentBunker = tempBunker;
+                    }
+                    else
+                    {
+                        nextBunker.Enqueue(tempBunker); //Every other bunker is added in the que
+                    }
+                }
+                else //We have a weapon
+                {
+                    var currentWeapon = int.Parse(element);
+
+                    if (currentBunker.Sum + currentWeapon <= totalCapacity) //The weapon can fit in the current bunker
+                    {
+                        currentBunker.Weapons.Add(currentWeapon);
+                        currentBunker.Sum += currentWeapon;
+                    }
+                    else
+                    {
+                        foreach (var bunker in bunkers) //We are searching bunker, that can fit the weapon
+                        {
+                            if (bunker.Sum + currentWeapon <= totalCapacity)//We found that bunker
+                            {
+                                bunker.Weapons.Add(currentWeapon);
+                                bunker.Sum += currentWeapon;
+                                foundBunker = true; //If this reamins false, we need to check current bunker for fitting the weapon
+                                break;
+                            }
+                        }
+
+                        if (foundBunker)
+                        {
+                            AddToPrint();
+                        }
+                        else
+                        {
+                            if (currentWeapon > totalCapacity) //If the weapon is bigger then the capacity, we dont check for fitting
+                            {
+                                AddToPrint();
+                            }
+                            else
+                            {
+                                currentBunker.Weapons.Add(currentWeapon);
+                                currentBunker.Sum += currentWeapon;
+
+                                currentBunker.Weapons.Reverse();
+
+                                for (int i = currentBunker.Weapons.Count - 1; i >= 0; i--)
+                                {
+                                    currentBunker.Sum -= currentBunker.Weapons[i];
+                                    currentBunker.Weapons.RemoveAt(i);
+
+                                    if (currentBunker.Sum <= totalCapacity)
+                                    {
+                                        break;
+                                    }
+                                }
+
+                                currentBunker.Weapons.Reverse();
+
+                            }
+                        }
+
+                        foundBunker = false;
+                    }
+                }
+            }
+
+            inputLine = Console.ReadLine();
+        }
+
+        Console.Write(result);
+    }
+
+    private static void AddToPrint()
+    {
+        var empty = "Empty";
+
+        if (currentBunker.Weapons.Count != 0)
+        {
+            empty = string.Join(", ", currentBunker.Weapons);
+        }
+
+        result.AppendLine($"{currentBunker.Name} -> {empty}");
+        currentBunker = nextBunker.Dequeue();
+    }
+}
+
+public class Bunker
+{
+    public string Name { get; set; }
+    public int Sum { get; set; }
+    public List<int> Weapons { get; set; }
+
+    public Bunker(string name, int sum, List<int> weapons)
+    {
+        this.Name = name;
+        this.Sum = sum;
+        this.Weapons = weapons;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {this.Name}, Sum: {this.Sum}, Weapons: {string.Join(", ", this.Weapons)}";
+    }
+}
---
+++ SoftUni 3.0/C#-Exams-and-Exam-Exercices/Exam-19-June-2016/03.Cubic-Messages/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Problem03
+{
+    static void Main()
+    {
+        var validMessage = true;
+        var result = new StringBuilder();
+
+        var message = Console.ReadLine();
+
+        while (!message.Equals("Over!"))
+        {
+            var count = int.Parse(Console.ReadLine());
+
+            var pattern = $"(.*)([a-zA-Z]{{{count}}})(.*)";
+            Regex rgx = new Regex(pattern);
+            MatchCollection matches = rgx.Matches(message);
+
+            foreach (Match match in matches)
+            {
+                foreach (var character in match.Groups[1].Value)
+                {
+                    if (!char.IsDigit(character))
+                    {
+                        validMessage = false;
+                        break;
+                    }
+                }
+
+                if (validMessage)
+                {
+                    foreach (var character in match.Groups[3].Value)
+                    {
+                        if (char.IsLetter(character))
+                        {
+                            validMessage = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (validMessage)
+                {
+                    var messageText = match.Groups[2].Value;
+                    var messageDigits = match.Groups[1].Value;
+
+                    if (match.Groups.Count == 4)
+                    {
+                        foreach (var character in match.Groups[3].Value)
+                        {
+                            if (char.IsDigit(character))
+                            {
+                                messageDigits += character;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+
+                    }
+
+                    result.Append($"{messageText} == ");
+                    foreach (var digit in messageDigits)
+                    {
+                        if (int.Parse(digit.ToString()) < messageText.Length)
+                        {
+                            result.Append(messageText[int.Parse(digit.ToString())]);
+                        }
+                        else
+                        {
+                            result.Append(" ");
+                        }
+                    }
+
+                    result.AppendLine();
+                }
+
+                validMessage = true;
+            }
+
+            message = Console.ReadLine();
+        }
+
+        Console.Write(result);
+    }
+}
