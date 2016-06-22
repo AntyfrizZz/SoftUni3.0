7e70882892c5378f4e82d75113400f415ff8338f
Yavor Milinov <iavor.milinov@gmail.com>
2016-06-22 18:02:43 +0300

    Exam-11-October-2015 added

---
+++ SoftUni 3.0/C#-Exams-and-Exam-Exercices/Exam-11-October-2015/04.Srabsko-Unleashed/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Startup
+{
+    static void Main()
+    {
+        var rgx = new Regex(@"(\D+)\s@(\D+)\s(\d+)\s(\d+)");
+
+        var eventsInfo = new Dictionary<string, Dictionary<string, int>>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "End")
+        {
+            var match = rgx.Match(input);
+            if (match.Success)
+            {
+                var singer = match.Groups[1].Value.Trim();
+                var town = match.Groups[2].Value.Trim();
+                var ticketPrice = int.Parse(match.Groups[3].Value);
+                var ticketsCount = int.Parse(match.Groups[4].Value);
+
+                if (!eventsInfo.ContainsKey(town))
+                {
+                    eventsInfo[town] = new Dictionary<string, int>();
+                }
+                if (!eventsInfo[town].ContainsKey(singer))
+                {
+                    eventsInfo[town][singer] = 0;
+                }
+
+                eventsInfo[town][singer] += ticketPrice * ticketsCount;
+            }
+        }
+
+        foreach (var pair in eventsInfo)
+        {
+            Console.WriteLine(pair.Key);
+            foreach (var concertInfo in pair.Value.OrderByDescending(c => c.Value))
+            {
+                Console.WriteLine("#  {0} -> {1}", concertInfo.Key, concertInfo.Value);
+            }
+        }
+    }
+}
---
+++ SoftUni 3.0/C#-Exams-and-Exam-Exercices/Exam-11-October-2015/README.md
@@ -0,0 +1,3 @@
+Advanced C# Exam 11 October 2015
+=========================================================
+https://judge.softuni.bg/Contests/Practice/Index/118#0
