517ad37f681ba48dd7e53998f0fe88e53ebddd28
Yavor Milinov <iavor.milinov@gmail.com>
2016-07-26 14:02:37 +0300

    Reflection Part of HW

---
+++ SoftUni 3.0/C#-Fundamentals-May-2016/C#-Advanced-OOP/Reflection/01HarestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,89 @@
+﻿namespace _01HarestingFields
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            var harvestingFields = typeof(HarvestingFields);
+            var fields = harvestingFields
+                .GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            List<FieldInfo> privateFields = new List<FieldInfo>();
+            List<FieldInfo> protectedFields = new List<FieldInfo>();
+            List<FieldInfo> publicFields = new List<FieldInfo>();
+
+            foreach (var fieldInfo in fields)
+            {
+                if (fieldInfo.IsPrivate)
+                {
+                    privateFields.Add(fieldInfo);
+                }
+                else if (fieldInfo.IsFamily)
+                {
+                    protectedFields.Add(fieldInfo);
+                }
+                else if (fieldInfo.IsPublic)
+                {
+                    publicFields.Add(fieldInfo);
+                }
+                else
+                {
+                    throw new Exception("Fields other than private, protected or public!");
+                }
+            }
+
+            var input = Console.ReadLine();
+
+            while (!input.Equals("HARVEST"))
+            {
+                switch (input)
+                {
+                    case "private":
+                        foreach (var privateField in privateFields)
+                        {
+                            Console.WriteLine($"private {privateField.FieldType.Name} {privateField.Name}");
+                        }
+                        break;
+                    case "protected":
+                        foreach (var protectedField in protectedFields)
+                        {
+                            Console.WriteLine($"protected {protectedField.FieldType.Name} {protectedField.Name}");
+                        }
+                        break;
+                    case "public":
+                        foreach (var publicField in publicFields)
+                        {
+                            Console.WriteLine($"public {publicField.FieldType.Name} {publicField.Name}");
+                        }
+                        break;
+                    case "all":
+                        foreach (var fieldInfo in fields)
+                        {
+                            var access = string.Empty;
+                            if (fieldInfo.IsPrivate)
+                            {
+                                access += "private";
+                            }
+                            else if (fieldInfo.IsFamily)
+                            {
+                                access += "protected";
+                            }
+                            else if (fieldInfo.IsPublic)
+                            {
+                                access += "public";
+                            }
+
+                            Console.WriteLine($"{access} {fieldInfo.FieldType.Name} {fieldInfo.Name}");
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SoftUni 3.0/C#-Fundamentals-May-2016/C#-Advanced-OOP/Reflection/02BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,44 @@
+﻿namespace _02BlackBoxInteger
+{
+    using System;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            var blackBox = typeof(BlackBoxInt);
+
+            var blackBoxConstructor = blackBox
+                .GetConstructor(
+                    BindingFlags.Instance | BindingFlags.NonPublic,
+                    null,
+                    Type.EmptyTypes,
+                    null);
+
+            var blackBoxInstance = (BlackBoxInt)blackBoxConstructor.Invoke(new object[] {});
+            var field = blackBox.GetField("innerValue", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var input = Console.ReadLine()
+                .Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+
+            while (!input[0].Equals("END"))
+            {
+                var operation = input[0];
+                var number = int.Parse(input[1]);
+
+                var method = blackBox
+                    .GetMethod(
+                        operation,
+                        BindingFlags.Instance | BindingFlags.NonPublic);
+
+                method.Invoke(blackBoxInstance, new object[] { number });
+
+                Console.WriteLine(field.GetValue(blackBoxInstance));
+
+                input = Console.ReadLine()
+                    .Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
